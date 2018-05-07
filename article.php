81b3a9def461f7fdb628964cb3e00958ab1a0621
dabauer <colinpseud@gmail.com>
2018-05-07 13:46:34 +0200

    afficher un article, afficher une liste darticles, requet par categorie

---
+++ article.php
@@ -0,0 +1,26 @@
+<?php
+
+  function affichearticle ($titre,$nom,$prenom,$date,$cat,$text) { //Renvoie une division avec un article
+      echo("<div id=\"header\">
+        <h2>".$titre."</h2>
+        <p>".$prenom." ".$nom."<br>".$date."<br>".$cat."</p>
+        <div id=\"corps\">".$text."</div>
+      </div>");
+  }
+
+  function listearticle ($res){ //Affiche une liste des articles de la requete sql
+    $ligne = mysqli_fetch_assoc($res);
+    while($ligne = mysqli_fetch_assoc($res)) {
+      affichearticle($ligne["titre"],$ligne["nom"],$ligne["prenom"],$ligne["timecreation"],$ligne["catnom"],$ligne["texte"]);
+    }
+  }
+
+//Il faut preciser la requete pour ne prendre que les colonnes necessaires
+  function requeteCat ($catid) { //Renvoie la requete pour les articles d'une categorie
+    $var = "SELECT titre, nom, prenom, timecreation, cat, texte FROM categorie, users, articles WHERE (user == userid) AND (cat == catid) AND (cat == ".$catid.") ORDER BY timecreation";
+    return $var;
+  }
+
+  affichearticle("titre","Bauer","David","2018","Actulaité","HRLLOWWORLD");
+  echo (requeteCat("2"));
+?>
